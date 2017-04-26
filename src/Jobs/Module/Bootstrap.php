1b435195c407962d5cc1f43680c5cd41905b4fbe
Dmitry Krokhin <nekufa@gmail.com>
2017-04-26 15:49:56 +0300

    refactored bootstrap

--- src/Jobs/Module/Bootstrap.php
+++ src/Jobs/Module/Bootstrap.php
@@ -5,6 +5,7 @@ namespace Basis\Jobs\Module;
 use Basis\Config;
 use Basis\Event;
 use Basis\Runner;
+use Exception;
 use LinkORB\Component\Etcd\Client;
 
 class Bootstrap
@@ -13,26 +14,38 @@ class Bootstrap
     {
         $runner->dispatch('tarantool.migrate');
 
-        $client->setRoot('services');
-        if(!$client->get($config['name'])) {
-            $client->set($config['name']);
-        }
+        $this->store($client, 'services/'.$config['name']);
 
-        $client->setRoot('jobs');
         $meta = $runner->dispatch('module.meta');
         foreach($meta['jobs'] as $job) {
-            if(!$client->get($job)) {
-                $client->set($job, $config['name']);
-            }
+            $this->store($client, 'jobs/'.$job, $config['name']);
         }
 
         if($config->offsetExists('events') && is_array($config['events'])) {
-            foreach($config['events'] as $event => $listeners) {
+            foreach($config['events'] as $nick => $listeners) {
                 $listeners = (array) $listeners;
                 foreach($listeners as $listener) {
-                    $event->subscribe($event, $listener);
+                    $event->subscribe($nick, $listener);
                 }
             }
+        }
+    }
+
+    private function store(Client $client, $path, $value = null)
+    {
+        list($folder, $key) = explode('/', $path);
+
+        $client->setRoot($folder);
+        try {
+            $client->ls('.');
+        } catch(Exception $e) {
+            $client->mkdir('.');
+        }
+
+        try {
+            $client->get($key);
+        } catch(Exception $e) {
+            $client->set($key, $value);
         }
     }
 }
