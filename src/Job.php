c005c863fcdcf0afa5c13e8fb05a493f98de4d10
Dmitry Krokhin <nekufa@gmail.com>
2017-06-22 11:53:57 +0300

    added abstract job

---
+++ src/Job.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Basis;
+
+use Tarantool\Mapper\Mapper;
+
+abstract class Job
+{
+    private $app;
+
+    public function __constructor(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    public function create($space, $data)
+    {
+        return $this->get(Mapper::class)->create($space, $data);
+    }
+
+    public function dispatch($job, $params = [])
+    {
+        return $this->app->dispatch($job, $params);
+    }
+
+    public function fire($event, $context)
+    {
+        return $this->get(Event::class)->fire($event, $context);
+    }
+
+    public function get($class)
+    {
+        return $this->app->get($class);
+    }
+
+    public function find($space, $params = [])
+    {
+        return $this->get(Mapper::class)->find($space, $params);
+    }
+
+    public function findOne($space, $params = [])
+    {
+        return $this->get(Mapper::class)->findOne($space, $params);
+    }
+}
