c3ecfebbcc565438073479b078288bf7cb86e2bd
Dmitry Krokhin <nekufa@gmail.com>
2017-06-28 20:36:26 +0300

    bootstrap fixed

--- src/Job/Module/Bootstrap.php
+++ src/Job/Module/Bootstrap.php
@@ -4,14 +4,19 @@ namespace Basis\Job\Module;
 
 use Basis\Job;
 use Exception;
+use Basis\Filesystem;
 
 class Bootstrap extends Job
 {
-    public function run()
+    public function run(Filesystem $fs)
     {
-        $this->dispatch('module.defaults');
-        $this->dispatch('module.register');
-
+        $cache = $fs->getPath('.cache');
+        if (is_dir($cache)) {
+            foreach ($fs->listFiles('.cache') as $file) {
+                unlink($fs->getPath('.cache/'.$file));
+            }
+            rmdir($cache);
+        }
         try {
             $this->dispatch('tarantool.migrate');
             $this->dispatch('tarantool.cache');
@@ -20,5 +25,8 @@ class Bootstrap extends Job
                 'migration' => $e->getMessage(),
             ];
         }
+
+        $this->dispatch('module.defaults');
+        $this->dispatch('module.register');
     }
 }
