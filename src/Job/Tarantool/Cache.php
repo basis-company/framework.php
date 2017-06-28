136e78eaa37275c7996181fed0ecc73f3080bdfb
Dmitry Krokhin <nekufa@gmail.com>
2017-06-28 13:55:29 +0300

    fixed cache permission

--- src/Job/Tarantool/Cache.php
+++ src/Job/Tarantool/Cache.php
@@ -10,10 +10,16 @@ class Cache
     public function run(Mapper $mapper, Filesystem $fs)
     {
         $filename = $fs->getPath('.cache/mapper-meta.php');
-        if (!is_dir(dirname($filename))) {
-            mkdir(dirname($filename));
+
+        $dir = dirname($filename);
+        if (!is_dir($dir)) {
+            mkdir($dir);
+            chown($dir, 'www-data');
+            chgrp($dir, 'www-data');
         }
 
         file_put_contents($filename, '<?php return '.var_export($mapper->getMeta(), true).';');
+        chown($filename, 'www-data');
+        chgrp($filename, 'www-data');
     }
 }
