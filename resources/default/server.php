77ed7bcba88a45d469ba05f525f32fd3db187a1f
Dmitry Krokhin <nekufa@gmail.com>
2017-12-15 09:08:30 +0300

    tz set http requests

--- resources/default/server.php
+++ resources/default/server.php
@@ -5,6 +5,8 @@ include 'vendor/autoload.php';
 use Basis\Application;
 use Basis\Http;
 
+date_default_timezone_set('Europe/Moscow');
+
 try {
     $app = new Application(__DIR__);
     echo $app->get(Http::class)->process($_SERVER['REQUEST_URI']);
