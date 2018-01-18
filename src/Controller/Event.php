56940bee026b677b3d0ef81df6a8e832dd092fd6
Dmitry Krokhin <nekufa@gmail.com>
2018-01-18 16:50:14 +0300

    event handler tune

--- src/Controller/Event.php
+++ src/Controller/Event.php
@@ -12,20 +12,7 @@ class Event
     public function index(Application $app, BasisEvent $event, Service $service)
     {
         try {
-            if (!array_key_exists('event', $_REQUEST)) {
-                throw new Exception('No event defined');
-            }
-
-            if (!array_key_exists('context', $_REQUEST)) {
-                throw new Exception('No context defined');
-            }
-
-            $context = json_decode($_REQUEST['context']);
-
-            if (!$context) {
-                throw new Exception('Invalid context');
-            }
-
+            $context = $this->getRequestContext();
             $subscription = $event->getSubscription();
 
             if (!array_key_exists($_REQUEST['event'], $subscription)) {
@@ -37,22 +24,47 @@ class Event
 
             foreach ($subscription[$_REQUEST['event']] as $listener) {
                 $instance = $app->get('Listener\\'.$listener);
-                foreach ($context as $k => $v) {
-                    $instance->$k = $v;
-                }
-                if (!method_exists($instance, 'run')) {
-                    throw new Exception('No run method for '.$class);
-                }
-
-                $result[$listener] = $app->call([$instance, 'run']);
+                $result[$listener] = $this->handleEvent($instance, $context);
             }
 
             return [
                 'success' => true,
                 'data' => $result,
             ];
+
         } catch (Exception $e) {
             return ['success' => false, 'message' => $e->getMessage()];
         }
+    }
+
+    private function getRequestContext()
+    {
+        if (!array_key_exists('event', $_REQUEST)) {
+            throw new Exception('No event defined');
+        }
+
+        if (!array_key_exists('context', $_REQUEST)) {
+            throw new Exception('No context defined');
+        }
+
+        $context = json_decode($_REQUEST['context']);
+
+        if (!$context) {
+            throw new Exception('Invalid context');
+        }
+
+        return $context;
+    }
+
+    private function handleEvent($instance, $context)
+    {
+        foreach ($context as $k => $v) {
+            $instance->$k = $v;
+        }
+        if (!method_exists($instance, 'run')) {
+            throw new Exception('No run method for '.$class);
+        }
+
+        return $app->call([$instance, 'run']);
     }
 }
