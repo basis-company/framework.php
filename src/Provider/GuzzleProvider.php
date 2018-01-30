8a526f0f14298fa428dbf485d29aa30ad3e1c472
Dmitry Krokhin <nekufa@gmail.com>
2018-01-30 15:23:33 +0300

    headers composing fix

--- src/Provider/GuzzleProvider.php
+++ src/Provider/GuzzleProvider.php
@@ -14,12 +14,17 @@ class GuzzleProvider extends AbstractServiceProvider
     public function register()
     {
         $this->getContainer()->share(Client::class, function () {
+            $headers = [
+                'transfer-encoding' => 'chunked',
+            ];
+            if (array_key_exists('HTTP_X_REAL_IP', $_SERVER)) {
+                $headers['x-real-ip'] = $_SERVER['HTTP_X_REAL_IP'];
+            }
+            if (array_key_exists('HTTP_X_SESSION', $_SERVER)) {
+                $headers['x-session'] = $_SERVER['HTTP_X_SESSION'];
+            }
             return new Client([
-                'headers' => [
-                    'transfer-encoding' => 'chunked',
-                    'x-real-ip' => $_SERVER['HTTP_X_REAL_IP'],
-                    'x-session' => $_SERVER['HTTP_X_SESSION'],
-                ]
+                'headers' => $headers
             ]);
         });
     }
