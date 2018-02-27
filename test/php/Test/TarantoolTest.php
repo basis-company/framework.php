488c42c4c3b62c3ae95fcbadd258b9b5f2682c5c
Dmitry Krokhin <nekufa@gmail.com>
2018-02-27 13:59:05 +0300

    extend tarantool test

--- test/php/Test/TarantoolTest.php
+++ test/php/Test/TarantoolTest.php
@@ -16,8 +16,34 @@ use Basis\Test;
 class TarantoolTest extends Test
 {
     public $data = [
-        'guard.session' => []
+        'guard.session' => [],
+        'web.services' => [
+            ['id' => 1, 'name' => 'tester'],
+            ['id' => 2, 'name' => 'basis'],
+            ['id' => 3, 'name' => 'web'],
+        ],
+        'tester.data' => [
+            ['id' => 3, 'value' => 'test'],
+            ['id' => 4, 'value' => 'test'],
+        ],
     ];
+
+    public $mocks = [
+        ['web.services', [], ['services' => ['web', 'tester', 'basis']]]
+    ];
+
+    public function testPool()
+    {
+        $web = $this->get(Pool::class)->get('web');
+        $services = $web->find('services');
+        $this->assertCount(3, $services);
+        $this->assertCount(1, $web->find('services', ['name' => 'web']));
+
+        $tester = $this->get(Pool::class)->get('tester');
+        $this->assertCount(0, $tester->find('data', ['id' => 2]));
+        $this->assertCount(1, $tester->find('data', ['id' => 3]));
+        $this->assertCount(2, $tester->find('data', ['value' => 'test']));
+    }
 
     public function tearDown()
     {
