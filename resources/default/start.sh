291dcd70137a2c74025da3ca5c70158b046a2e19
Dmitry Krokhin <dmitry.krokhin@basis.company>
2019-10-09 18:38:59 +0300

    dump optimizer loader on start

--- resources/default/start.sh
+++ resources/default/start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+./composer.phar dump-autoload -o
+vendor/bin/console module.bootstrap
+
 if [[ ! -z "${BASIS_JOB}" ]]; then
-  ./vendor/bin/console module.bootstrap
   while true; do
-    ./vendor/bin/console module.runner
+    vendor/bin/console module.runner
   done
 else
   echo "ServerName $SERVICE_NAME" > /etc/apache2/conf-enabled/server-name.conf
-  ./vendor/bin/console module.bootstrap
   apache2-foreground
 fi
