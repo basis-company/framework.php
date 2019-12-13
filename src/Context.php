a86d6fa61e5ef3e61ccc4bfd487b9096ad0a59c3
Dmitry Krokhin <dmitry.krokhin@basis.company>
2019-12-13 23:18:44 +0300

    context serialization

--- src/Context.php
+++ src/Context.php
@@ -30,6 +30,9 @@ class Context
     public function apply($data) : self
     {
         foreach ($data as $k => $v) {
+            if ($k == 'parent') {
+                $v = (object) $v;
+            }
             $this->$k = $v;
         }
 
@@ -39,5 +42,28 @@ class Context
     public function getPerson()
     {
         return $this->parent ? $this->parent->person : $this->person;
+    }
+
+    public function toArray(): array
+    {
+        $result = [];
+        foreach (get_object_vars($this) as $k => $v) {
+            if (!is_null($v)) {
+                if ($k == 'parent') {
+                    $result[$k] = [];
+                    foreach ($v as $kk => $vv) {
+                        if ($vv) {
+                            $result[$k][$kk] = $vv;
+                        }
+                    }
+                    if (!count($result[$k])) {
+                        unset($result[$k]);
+                    }
+                } else {
+                    $result[$k] = $v;
+                }
+            }
+        }
+        return $result;
     }
 }
