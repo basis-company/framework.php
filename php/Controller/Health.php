cf0c1d0ca44eb0e10cdd5cb3f77856630b168b4c
Dmitry Krokhin <nekufa@gmail.com>
2020-07-24 15:07:56 +0300

    background hold based healthcheck

--- php/Controller/Health.php
+++ php/Controller/Health.php
@@ -2,10 +2,21 @@
 
 namespace Basis\Controller;
 
+use Basis\Metric\BackgroundHold;
+
 class Health
 {
-    public function index()
+    public function index(BackgroundHold $hold)
     {
+        // 5 minutes default limit
+        $threshold = getenv('BACKGROUND_HOLD_MAX') ?: 5 * 60;
+
+        // validate current hold value
+        if ($hold->getValue() >= $threshold) {
+            return;
+        }
+
+        // all is correct
         return 'ok';
     }
 }
