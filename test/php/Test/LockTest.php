0505ffbb756ca73abb634cb898f8242220d656c9
Dmitry Krokhin <nekufa@gmail.com>
2020-08-04 14:11:45 +0300

    tear down test

--- test/php/Test/LockTest.php
+++ test/php/Test/LockTest.php
@@ -7,6 +7,15 @@ use Basis\Test;
 
 class LockTest extends Test
 {
+    public function testRelease()
+    {
+        $lock = $this->get(Lock::class);
+        $lock->acquire('tester');
+        $this->tearDown();
+
+        $this->assertFalse($lock->exists('tester'));
+    }
+
     public function test()
     {
         $lock = $this->get(Lock::class);
