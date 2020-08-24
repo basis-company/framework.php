c919441ba919d2e5d6d59227ad38c74a6b7b020b
Dmitry Krokhin <nekufa@gmail.com>
2020-08-24 20:11:22 +0300

    tune http server configuration

--- php/Configuration/Swoole.php
+++ php/Configuration/Swoole.php
@@ -18,10 +18,15 @@ class Swoole
 
             $server = new Server($host, $port);
             $server->set([
-                'buffer_output_size' => 128 * 1024 * 1024,
+                'buffer_output_size' => getenv('SWOOLE_HTTP_SERVER_BUFFER_OUTPUT_SIZE') ?: 128 * 1024 * 1024,
                 'document_root' => getcwd(),
+                'enable_coroutine' => true,
                 'enable_static_handler' => true,
+                'http_compression' => false,
+                'dispatch_mode' => getenv('SWOOLE_HTTP_SERVER_DISPATCH_MODE') ?: 3,
                 'http_parse_post' => true,
+                'max_request' => getenv('SWOOLE_HTTP_SERVER_MAX_REQUEST') ?: 64,
+                'worker_num' => getenv('SWOOLE_HTTP_SERVER_WORKER_NUM') ?: 4,
             ]);
 
             $server->on("start", function () use ($container, $host, $port) {
