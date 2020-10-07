2ab787248bf1937d7862d1d70c73367672562b3e
Dmitry Krokhin <nekufa@gmail.com>
2020-10-07 11:59:40 +0300

    process coroutine sleep exception

--- php/Job/Module/Sleep.php
+++ php/Job/Module/Sleep.php
@@ -4,6 +4,7 @@ namespace Basis\Job\Module;
 
 use Basis\Job;
 use Swoole\Coroutine;
+use Swoole\Error;
 
 class Sleep extends Job
 {
@@ -12,7 +13,11 @@ class Sleep extends Job
     public function run()
     {
         if (class_exists(Coroutine::class)) {
-            return Coroutine::sleep($this->seconds);
+            try {
+                return Coroutine::sleep($this->seconds);
+            } catch (Error $error) {
+                // API must be called in the coroutine
+            }
         }
 
         return sleep($this->seconds);
