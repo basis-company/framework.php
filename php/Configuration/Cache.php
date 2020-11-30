5a91a13115dce8b27d36454ab3d6b01a7f214046
Dmitry Krokhin <nekufa@gmail.com>
2020-11-30 12:44:35 +0300

    cache with filesystem layer

--- php/Configuration/Cache.php
+++ php/Configuration/Cache.php
@@ -6,6 +6,8 @@ use Basis\Container;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\Cache\Adapter\ApcuAdapter;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Adapter\ChainAdapter;
+use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
 
 class Cache
 {
@@ -19,10 +21,27 @@ class Cache
             return new ArrayAdapter();
         });
 
-        if (in_array('apcu', get_loaded_extensions())) {
-            $container->share(AdapterInterface::class, ApcuAdapter::class);
-        } else {
+        $container->share(PhpFilesAdapter::class, function () {
+            return new PhpFilesAdapter('', 0, 'cache');
+        });
+
+        if (getenv('SERVICE_ENVIRONMENT') === 'testing') {
             $container->share(AdapterInterface::class, ArrayAdapter::class);
+        } elseif (in_array('apcu', get_loaded_extensions())) {
+            $container->share(AdapterInterface::class, function () use ($container) {
+                return new ChainAdapter([
+                    $container->get(ArrayAdapter::class),
+                    $container->get(ApcuAdapter::class),
+                    $container->get(PhpFilesAdapter::class),
+                ]);
+            });
+        } else {
+            $container->share(AdapterInterface::class, function () use ($container) {
+                return new ChainAdapter([
+                    $container->get(ArrayAdapter::class),
+                    $container->get(PhpFilesAdapter::class),
+                ]);
+            });
         }
     }
 }
