327c282997bcc7cde544ce169c7c24d701d7782c
dmitry krokhin <nekufa@gmail.com>
2021-03-02 15:10:25 +0300

    job queue recover on processing timeout

--- php/Job/Module/Recover.php
+++ php/Job/Module/Recover.php
@@ -9,17 +9,31 @@ class Recover
 {
     use Toolkit;
 
+    public int $timeout = 5 * 60;
+
     public function run()
     {
-        $this->getMapper()
-            ->getClient()
-            ->evaluate(<<<LUA
-                box.begin()
-                box.space.job_queue.index.status_id:pairs('processing')
-                    :each(function(t)
-                      box.space.job_queue:update(t.id, {{'=', 2, 'new'}})
-                    end)
-                box.commit()
-                LUA);
+        $script = <<<LUA
+            box.begin()
+            local new = box.space.job_queue.index.status_id:pairs('processing')
+                :filter(function(t) return t[9] == nil end)
+                :map(function(t)
+                    return box.space.job_queue:update(t.id, {{'=', 9, { since = TIMESTAMP }}})
+                end)
+                :totable()
+            local recovered = box.space.job_queue.index.status_id:pairs('processing')
+                :filter(function(t) return t[9] ~= nil and t[9]['since'] <= TIMEOUT end)
+                :map(function(t)
+                    return box.space.job_queue:update(t.id, {{'=', 2, 'new'}, { '=', 9, box.NULL}})
+                end)
+                :totable()
+            box.commit()
+            return { new = #new, recovered = #recovered }
+        LUA;
+
+        $script = str_replace('TIMESTAMP', time(), $script);
+        $script = str_replace('TIMEOUT', time() - $this->timeout, $script);
+
+        return $this->getMapper()->getClient()->evaluate($script)[0];
     }
 }
--- test/php/Test/RunnerTest.php
+++ test/php/Test/RunnerTest.php
@@ -3,12 +3,94 @@
 namespace Test;
 
 use Basis\Job\Job\Info;
+use Basis\Job\Module\Recover;
 use Basis\Test;
 use Exception;
 use Job\Hello;
+use Tarantool\Client\Schema\Criteria;
+use Tarantool\Client\Schema\Operations;
 
 class RunnerTest extends Test
 {
+    public function testRecover()
+    {
+        $this->send('hello.world');
+
+        $queue = $this->findOrFail('job_queue');
+        $this->assertSame($queue->status, 'new');
+
+        $queue->status = 'processing';
+        $queue->save();
+
+        $space = $this->getMapper()
+            ->getClient()
+            ->getSpace('job_queue');
+
+        $criteria = Criteria::key([$queue->id]);
+        [$tuple] = $space->select($criteria);
+
+        $this->assertCount(8, $tuple);
+
+        // since timestamp was set
+        $recover = $this->dispatch('module.recover');
+        $this->assertSame($recover->new, 1);
+        $this->assertSame($recover->recovered, 0);
+
+        // no changes
+        $recover = $this->dispatch('module.recover');
+        $this->assertSame($recover->new, 0);
+        $this->assertSame($recover->recovered, 0);
+
+        $criteria = Criteria::key([$queue->id]);
+        [$tuple] = $space->select($criteria);
+
+        $past = $tuple[8]['since'] - $this->get(Recover::class)->timeout + 1;
+        $space->update([$queue->id], Operations::set(8, [ 'since' => $past ]));
+
+        // still no changes
+        $recover = $this->dispatch('module.recover');
+        $this->assertSame($recover->new, 0);
+        $this->assertSame($recover->recovered, 0);
+
+        $past = $tuple[8]['since'] - $this->get(Recover::class)->timeout;
+        $space->update([$queue->id], Operations::set(8, [ 'since' => $past ]));
+
+        // recovered job
+        $recover = $this->dispatch('module.recover');
+        $this->assertSame($recover->new, 0);
+        $this->assertSame($recover->recovered, 1);
+
+        [$tuple] = $space->select($criteria);
+        $this->assertCount(9, $tuple);
+        $this->getRepository('job_queue')->sync($queue->id);
+        $this->assertSame($queue->status, 'new');
+
+        // no more changes
+        $recover = $this->dispatch('module.recover');
+        $this->assertSame($recover->new, 0);
+        $this->assertSame($recover->recovered, 0);
+
+        $criteria = Criteria::key([$queue->id]);
+        [$tuple] = $space->select($criteria);
+
+        $this->assertNull($tuple[8]);
+
+        $queue->status = 'processing';
+        $queue->save();        
+
+        // add timestamp
+        $recover = $this->dispatch('module.recover');
+        $this->assertSame($recover->new, 1);
+        $this->assertSame($recover->recovered, 0);
+
+        $criteria = Criteria::key([$queue->id]);
+        [$tuple] = $space->select($criteria);
+
+        $this->assertNotNull($tuple[8]);
+
+    }
+
+
     public function test()
     {
         $result = $this->dispatch('test.hello');
