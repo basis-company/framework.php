85e8cc51904f4d1fd35d1b92fedee2b18d29ba5f
dmitry krokhin <nekufa@gmail.com>
2022-01-27 14:00:17 +0300

    fully-qualified job names only

--- php/Job/Module/Meta.php
+++ php/Job/Module/Meta.php
@@ -8,14 +8,18 @@ use Basis\Http;
 
 class Meta
 {
+    public bool $filter = true;
+
     public function run(Application $app, Http $http, Dispatcher $dispatcher)
     {
         $jobs = [];
+
         foreach ($dispatcher->getJobs() as $job => $class) {
-            if (strpos($job, $app->getName() . '.') !== false) {
+            if (!$this->filter || strpos($job, $app->getName() . '.') !== false) {
                 $jobs[] = $job;
             }
         }
+
         return [
             'routes' => $http->getRoutes(),
             'jobs' => $jobs,
