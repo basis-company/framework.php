92d8ef2cdde6cf9003d7fff2451ff490e6885893
dmitry krokhin <nekufa@gmail.com>
2022-02-07 14:15:13 +0300

    tune stream configuration

--- php/Job/Nats/Migrate.php
+++ php/Job/Nats/Migrate.php
@@ -17,6 +17,7 @@ class Migrate
         foreach ($dispatcher->getHandlers() as $handler) {
             $stream = $client->getApi()->getStream($handler['subject']);
             $stream->getConfiguration()
+                   ->setDenyDelete(false)
                    ->setDiscardPolicy(DiscardPolicy::NEW)
                    ->setRetentionPolicy(RetentionPolicy::WORK_QUEUE)
                    ->setStorageBackend(StorageBackend::FILE)
