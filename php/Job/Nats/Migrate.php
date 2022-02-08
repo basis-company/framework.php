846a108e8657ddecf1d82cf0647c825dfd178b7c
dmitry krokhin <nekufa@gmail.com>
2022-02-08 14:16:32 +0300

    optional stream configuration for storage backend and duplicate window

--- php/Job/Nats/Migrate.php
+++ php/Job/Nats/Migrate.php
@@ -17,12 +17,23 @@ class Migrate
         foreach ($dispatcher->getHandlers() as $handler) {
             $stream = $client->getApi()->getStream($handler['subject']);
             $stream->getConfiguration()
-                    ->setAllowRollupHeaders(true)
+                   ->setAllowRollupHeaders(true)
                    ->setDenyDelete(false)
                    ->setDiscardPolicy(DiscardPolicy::NEW)
                    ->setRetentionPolicy(RetentionPolicy::WORK_QUEUE)
                    ->setStorageBackend(StorageBackend::FILE)
                    ->setSubjects([$handler['subject']]);
+
+            if (array_key_exists('configuration', $handler)) {
+                if (array_key_exists('storage_backend', $handler['configuration'])) {
+                    $stream->getConfiguration()
+                        ->setStorageBackend($handler['configuration']['storage_backend']);
+                }
+                if (array_key_exists('duplicate_window', $handler['configuration'])) {
+                    $stream->getConfiguration()
+                        ->setDuplicateWindow($handler['configuration']['duplicate_window']);
+                }
+            }
 
             if (!$stream->exists()) {
                 $stream->create();
