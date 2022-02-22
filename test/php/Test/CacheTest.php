83a3b299166df7dfafa3ba55642eeb43a622e1f8
dmitry krokhin <nekufa@gmail.com>
2022-02-22 14:20:51 +0300

    cache test fix

--- test/php/Test/CacheTest.php
+++ test/php/Test/CacheTest.php
@@ -13,6 +13,8 @@ class CacheTest extends Test
         $cache->set('username', 'nekufa');
 
         $this->assertSame($cache->get('username'), 'nekufa');
+
+        $cache->clear();
         $this->assertNull($cache->get('username'));
     }
 }
