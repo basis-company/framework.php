45c394fe4895f8177acc641088a253e635f5bd7f
dmitry krokhin <nekufa@gmail.com>
2022-02-22 14:47:12 +0300

    dispatcher cache test

---
+++ test/php/Job/Random.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Job;
+
+use Basis\Job;
+
+class Random extends Job
+{
+    public function run()
+    {
+        return [
+            'expire' => PHP_INT_MAX,
+            'value' => bin2hex(random_bytes(8)),
+        ];
+    }
+}
--- test/php/Test/CacheTest.php
+++ test/php/Test/CacheTest.php
@@ -17,4 +17,19 @@ class CacheTest extends Test
         $cache->clear();
         $this->assertNull($cache->get('username'));
     }
+
+    public function testDispatcherCache()
+    {
+        $random1 = $this->dispatch('test.random');
+        $random2 = $this->dispatch('test.random');
+        $this->assertSame($random1->value, $random2->value);
+
+        $this->get(Cache::class)->clear();
+
+        $random2 = $this->dispatch('test.random');
+        $this->assertNotSame($random1->value, $random2->value);
+
+        $random1 = $this->dispatch('test.random');
+        $this->assertSame($random1->value, $random2->value);
+    }
 }
