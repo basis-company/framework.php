1655a1efbd2037606ccd0f20ceb8c53378a5b74d
dmitry krokhin <nekufa@gmail.com>
2022-12-30 13:13:21 +0300

    key and token using cache api

--- php/Application.php
+++ php/Application.php
@@ -60,45 +60,30 @@ class Application
 
     public function getToken()
     {
-        try {
-            if (!file_exists('/tmp/token.php')) {
-                throw new Exception("Initialize", 1);
-            }
-
-            $token = include '/tmp/token.php';
-            $payload = $this->getTokenPayload($token);
-            if ($payload->iat < time() + 60) {
-                throw new Exception("Token is about to expire", 1);
-            }
-        } catch (Throwable $e) {
+        $cached = $this->get(Cache::class)->wrap('service-token', function () {
             $token = $this->findOrFail('guard.token', ['service' => $this->getName()])->token;
-            file_put_contents('/tmp/token.php', '<?php return "' . $token . '";');
-        }
+            $expire = $this->getTokenPayload($token)->iat - 60;
+            return (object) compact('expire', 'token');
+        });
 
-        return $token;
+        return $cached->token;
     }
 
     public function getTokenPayload(string $token): object
     {
-        $key = null;
-        if (file_exists('resources/jwt/public')) {
-            // guard
-            $key = file_get_contents('resources/jwt/public');
-        } elseif (file_exists('key')) {
-            // cached key
-            $key = file_get_contents('key');
-        } else {
-            // others
-            $key = file_get_contents('http://guard/guard/key');
-            if ($key) {
-                file_put_contents('key', $key);
+        $cached = $this->get(Cache::class)->wrap('public-key', function () {
+            if (file_exists('resources/jwt/public')) {
+                $key = file_get_contents('resources/jwt/public');
+            } else {
+                $key = file_get_contents('http://guard/guard/key');
             }
-        }
+            if (!$key) {
+                throw new Exception("Key calculation failure");
+            }
+            $expire = time() + 365 * 24 * 60 * 60;
+            return (object) compact('expire', 'key');
+        });
 
-        if (!$key) {
-            throw new Exception("Key calculation failure");
-        }
-
-        return JWT::decode($token, new Key($key, 'RS256'));
+        return JWT::decode($token, new Key($cached->key, 'RS256'));
     }
 }
